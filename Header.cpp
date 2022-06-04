76f0a17f49ff7edd887cdb2437231bf7653a10d4
Caruso <jpfcaruso@gmail.com>
2022-06-03 22:26:52 -0300

    Inicio do projeto

---
+++ Header.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <utility>      
+#include <vector> 
+#include <set>
+#include <map>
+#include <math.h> 
+#include <algorithm> 
+#include <new>   
+#include <unordered_map> 
+#include <iomanip>
+#include <queue>
+using namespace std;
+
